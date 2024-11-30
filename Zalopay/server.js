ec91dab050a5b7f7122b549c1bb065c6f0fafa91
damphu1102 <damphu207@gmail.com>
2024-11-30 20:37:56 +0700

    updated

--- Zalopay/server.js
+++ Zalopay/server.js
@@ -137,49 +137,49 @@ app.post('/callback', (req, res) => {
  * nên Merchant cần hiện thực việc chủ động gọi API truy vấn trạng thái đơn hàng.
  */
 
-app.post('/check-status-order', async (req, res) => {
-  const { app_trans_id } = req.body;
+// app.post('/check-status-order', async (req, res) => {
+//   const { app_trans_id } = req.body;
 
-  let postData = {
-    app_id: config.app_id,
-    app_trans_id, // Input your app_trans_id
-  };
+//   let postData = {
+//     app_id: config.app_id,
+//     app_trans_id, // Input your app_trans_id
+//   };
 
-  let data = postData.app_id + '|' + postData.app_trans_id + '|' + config.key1; // appid|app_trans_id|key1
-  postData.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
+//   let data = postData.app_id + '|' + postData.app_trans_id + '|' + config.key1; // appid|app_trans_id|key1
+//   postData.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
 
-  let postConfig = {
-    method: 'post',
-    url: 'https://sb-openapi.zalopay.vn/v2/query',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    data: qs.stringify(postData),
-  };
+//   let postConfig = {
+//     method: 'post',
+//     url: 'https://sb-openapi.zalopay.vn/v2/query',
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     data: qs.stringify(postData),
+//   };
 
-  try {
-    const result = await axios(postConfig);
-    console.log(result.data);
-    return res.status(200).json(result.data);
-    /**
-     * kết quả mẫu
-      {
-        "return_code": 1, // 1 : Thành công, 2 : Thất bại, 3 : Đơn hàng chưa thanh toán hoặc giao dịch đang xử lý
-        "return_message": "",
-        "sub_return_code": 1,
-        "sub_return_message": "",
-        "is_processing": false,
-        "amount": 50000,
-        "zp_trans_id": 240331000000175,
-        "server_time": 1711857138483,
-        "discount_amount": 0
-      }
-    */
-  } catch (error) {
-    console.log('lỗi');
-    console.log(error);
-  }
-});
+//   try {
+//     const result = await axios(postConfig);
+//     console.log(result.data);
+//     return res.status(200).json(result.data);
+//     /**
+//      * kết quả mẫu
+//       {
+//         "return_code": 1, // 1 : Thành công, 2 : Thất bại, 3 : Đơn hàng chưa thanh toán hoặc giao dịch đang xử lý
+//         "return_message": "",
+//         "sub_return_code": 1,
+//         "sub_return_message": "",
+//         "is_processing": false,
+//         "amount": 50000,
+//         "zp_trans_id": 240331000000175,
+//         "server_time": 1711857138483,
+//         "discount_amount": 0
+//       }
+//     */
+//   } catch (error) {
+//     console.log('lỗi');
+//     console.log(error);
+//   }
+// });
 
 app.listen(8888, function () {
   console.log('Server is listening at port :8888');
